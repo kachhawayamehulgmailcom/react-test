9a3c74832191765cbf52ec783d5ccd1270f7398b
Mehul <kachhwayamehul@gmail.com>
2020-08-15 21:27:50 +0530

    Updates

---
+++ precache-manifest.4ede4b1ab722426717bfab2e65c19165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12acbd4268ce0430c4b44ad7bafadbe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7f1dd4b5d4bfb0dd3e",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "e4ecfc19fed1301d354a",
+    "url": "/static/css/main.e96a6d53.chunk.css"
+  },
+  {
+    "revision": "ef7f1dd4b5d4bfb0dd3e",
+    "url": "/static/js/2.5c3bc276.chunk.js"
+  },
+  {
+    "revision": "e4ecfc19fed1301d354a",
+    "url": "/static/js/main.a9e4a419.chunk.js"
+  },
+  {
+    "revision": "e91e9ffd41f58ed1ba6b",
+    "url": "/static/js/runtime~main.96cf63f4.js"
+  }
+]);
